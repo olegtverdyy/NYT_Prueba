3a1ed5f790f1ef6ba1087be61cbaf1760a2829b7
Oleg Tverdyy <oleg.tverdyy@sdos.es>
2021-08-10 10:51:16 +0200

    - Main screen

---
+++ NYT/view/controller/main/MainClasses.swift
@@ -0,0 +1,37 @@
+//
+//  MainClasses.swift
+//  NYT
+//
+//  Created by Oleg Tverdyy on 10/8/21.
+//
+
+import Foundation
+
+public enum ArticleType: Int {
+    case mostEmailed = 0
+    case mostShared
+    case mostViewed
+    
+    func stringValue() -> String {
+        switch self {
+        case .mostEmailed: return "mostmailed"
+        case .mostShared: return "mostshared"
+        case .mostViewed: return "mostviewed"
+        }
+    }
+}
+
+public enum DaysPeriod: Int {
+    case one = 0
+    case seven
+    case thirty
+    
+    func stringValue() -> String {
+        switch self {
+        case .one: return "1"
+        case .seven: return "7"
+        case .thirty: return "30"
+        }
+    }
+}
+
---
+++ NYT/view/controller/main/MainViewController.swift
@@ -0,0 +1,83 @@
+//
+//  MainViewController.swift
+//  NYT
+//
+//  Created by Oleg Tverdyy on 10/8/21.
+//
+
+import UIKit
+import MultiSelectSegmentedControl
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var segmentArticleType: UISegmentedControl!
+    @IBOutlet weak var segmentDaysPeriod: UISegmentedControl!
+    @IBOutlet weak var segmentSocialType: MultiSelectSegmentedControl!
+    @IBOutlet weak var socialTypeContainer: UIView!
+    
+    private var articleType: ArticleType = .mostEmailed
+    private var daysPeriod: DaysPeriod = .one
+    private var socialNetworks: [String] = []
+    
+    private var posibleNetworks: [String] = ["facebook", "twitter"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Buscador"
+        
+        setup()
+        checkMostShared()
+    }
+    
+    func setup() {
+        segmentSocialType.items = ["Facebook", "Twitter"]
+        segmentSocialType.delegate = self
+    }
+    
+    func checkMostShared() {
+        socialTypeContainer.isHidden = articleType != .mostShared
+        
+        // Deselect all items previusly selected
+        if socialTypeContainer.isHidden {
+            socialNetworks.removeAll()
+            segmentSocialType.selectedSegmentIndexes = []
+        }
+    }
+    
+}
+
+// MARK: - Actions
+
+extension MainViewController {
+    @IBAction func actionArticleType(_ sender: Any) {
+        if let segment = sender as? UISegmentedControl, let type = ArticleType(rawValue: segment.selectedSegmentIndex) {
+            articleType = type
+            checkMostShared()
+        }
+    }
+    
+    @IBAction func actionDaysPeriod(_ sender: Any) {
+        if let segment = sender as? UISegmentedControl, let days = DaysPeriod(rawValue: segment.selectedSegmentIndex) {
+            daysPeriod = days
+        }
+    }
+    
+    @IBAction func actionSearch(_ sender: Any) {
+        print("\(articleType.stringValue()) - \(daysPeriod.stringValue()) - \(socialNetworks)")
+    }
+}
+
+// MARK: - MultiSegmentDelegate
+
+extension MainViewController: MultiSelectSegmentedControlDelegate {
+    func multiSelect(_ multiSelectSegmentedControl: MultiSelectSegmentedControl, didChange value: Bool, at index: Int) {
+        if value {
+            socialNetworks.append(posibleNetworks[index])
+        } else {
+            if socialNetworks.contains(posibleNetworks[index]) {
+                socialNetworks.removeAll { $0 == posibleNetworks[index] }
+            }
+        }
+    }
+}
